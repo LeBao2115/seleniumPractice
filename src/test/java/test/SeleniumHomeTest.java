d835987fe565e2166b2152df0027f90223f46043
LeBao2115 <lephuanhbao1042001@gmail.com>
2025-04-21 14:37:33 +0700

    POM design pattern

--- src/test/java/test/SeleniumHomeTest.java
+++ src/test/java/test/SeleniumHomeTest.java
@@ -1,18 +1,26 @@
 package test;
 
 import base.BaseTest;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import page.HomePage;
 import page.SeleniumHomePage;
 
 public class SeleniumHomeTest extends BaseTest {
+    private HomePage homePage;
+    private SeleniumHomePage seleniumHomePage;
+
+    @BeforeMethod
+    public void setUpPages() {
+        // Initialize page objects here, after webDriver is set up in BaseTest
+        homePage = new HomePage(webDriver);
+        seleniumHomePage = new SeleniumHomePage(webDriver);
+    }
+
     @Test
     public void testNavigatePracticeWeb(){
-        HomePage homePage = new HomePage(webDriver);
         homePage.navigateSeleniumPage();
         takeScreenshot("Move to Selenium Home");
-
-        SeleniumHomePage seleniumHomePage = new SeleniumHomePage(webDriver);
         seleniumHomePage.navigatePracticeWeb();
         takeScreenshot("Move to Practice Web");
     }
